857071d3b8f19a6020d1c3688e3cd5354e3f70d8
Shreeyash09 <shreeyashsalunkhe@gmail.com>
2023-12-28 10:34:41 +0530

    Added subnet configuration in existing code

--- cdk-vpc/vpc/vpc_stack.py
+++ cdk-vpc/vpc/vpc_stack.py
@@ -12,9 +12,32 @@ class VpcStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         vpc = ec2.Vpc(self, "Vpc",
-            ip_addresses=ec2.IpAddresses.cidr("10.10.0.0/16"),
+            ip_addresses=ec2.IpAddresses.cidr("169.10.0.0/16"),
             max_azs=2,
             nat_gateways=1,
             reserved_azs=0,
-            vpc_name="Shreeyash-cdk-vpc" 
+            vpc_name="Shreeyash-cdk-vpc",
+            enable_dns_hostnames=True,
+            enable_dns_support=True,
+            subnet_configuration=[
+                # Public subnets
+                ec2.SubnetConfiguration(
+                    name="snet-public-subnet-01",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=22,
+                    map_public_ip_on_launch=True,
+                ),
+                # Private with internet subnets
+                ec2.SubnetConfiguration(
+                    name="snet-private-internet-subnet-01",
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT,
+                    cidr_mask=22,
+                ),
+                # Private subnet
+                ec2.SubnetConfiguration(
+                    name="snet-private-subnet-01",
+                    subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                    cidr_mask=22,
+                ),
+            ],
         )
